3a0f5a479e82651e057a30383fc8a7bee61bb8ab
IHATETHEGOVERNMENT <156684018+IHATETHEGOVERNMENT@users.noreply.github.com>
2024-01-15 15:20:13 +0100

    Add files via upload

---
+++ ex01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int o = 119;
+    float C = 1.19119119119;
+    double c = 1.19119119119119119;
+    char a = 119;
+    long int z = 11911911911911911;
+    short Z = 1;
+    unsigned O = -119;
+printf("int %d %f %f %c %ld %hd %u \n", o, C, c, a, z, Z, O);
+return(0);
+
+}
