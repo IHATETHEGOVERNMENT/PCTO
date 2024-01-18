5ea8e9b2bd8d093f3ae94f51792a85bbf5723529
CAAZZZZZOTELEGGIZINGHIRO <156684018+IHATETHEGOVERNMENT@users.noreply.github.com>
2024-01-18 17:08:41 +0100

    Add files via upload

---
+++ ex26.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(){
+    double x;
+    double y;
+    char a;
+    double somma;
+    double sottrazione;
+    double divisione;
+    double moltiplicazione;
+    char così;
+
+     while((così!='+')||(così!='-')||(così!='/')||(così!='*')){
+    printf("bella fra inserisci i valori dei coefficienti \n");
+    scanf("%lf %lf", &x , &y );
+    printf("che operazione ce voi svorge? (puoi scegliere tra +, -, /, *)\n");
+    scanf(" %c" ,&a);
+    if (a=='+')
+    {
+        somma = x + y;
+        printf("il risultato è %lf\n" , somma);
+    }
+    if (a=='-'){
+        sottrazione = x-y;
+        printf("il risultato è %lf\n" , sottrazione);
+    }
+    if (a=='/'){
+        divisione = x/y;
+        printf("il risultato dell'operazione è %lf\n" , divisione);
+    }
+    if (a=='*'){
+        moltiplicazione = x*y;
+        printf("il risultato dell'operazione è %lf\n" , moltiplicazione);
+    }
+    else {
+        printf("cojone ho detto un numero\n");
+    }
+    }
+    return 0;
+}
