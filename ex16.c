16993ddaa6cc5af9a4f58f073d6275de328d5307
CAAZZZZZOTELEGGIZINGHIRO <156684018+IHATETHEGOVERNMENT@users.noreply.github.com>
2024-01-18 09:23:41 +0100

    Add files via upload

---
+++ ex16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void cheneso(){
+    char c;
+    char r;
+    char o;
+    char k;
+
+    printf("che squadra tifi?\n");
+    scanf("%c %c %c %c", c, r, o, k );
+    cubo =  *  * ;
+    printf("ok damme %d patate e %d euro\n",nummeroo, cubo);
+}
+
+int main() {
+    cheneso();
+    cheneso();
+    cheneso();
+    cheneso();
+    return(0);
+}
