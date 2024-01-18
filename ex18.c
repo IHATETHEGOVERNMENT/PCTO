702dc9814fb47e13502ad23a34b2618b953fa78b
CAAZZZZZOTELEGGIZINGHIRO <156684018+IHATETHEGOVERNMENT@users.noreply.github.com>
2024-01-18 09:24:34 +0100

    Add files via upload

---
+++ ex18.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    int ;
+    int ;
+        printf("Inserisci un numero:\n");
+        scanf("%d", &a);
+    while(i%b!=0) {
+        b - 1;
+    }
+    return 0;
+}
