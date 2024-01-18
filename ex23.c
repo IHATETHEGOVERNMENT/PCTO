1db482ac98941148f0d3743b1396f3632a642382
CAAZZZZZOTELEGGIZINGHIRO <156684018+IHATETHEGOVERNMENT@users.noreply.github.com>
2024-01-18 09:26:22 +0100

    Add files via upload

---
+++ ex23.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main(){
+    int n;
+    int b = 2;
+        printf("inserire un numero\n");
+        scanf("%d" , &n);
+        printf("la media è: %d\n", n);
+    while(n>=0){
+
+        printf("inserire un numero\n");
+        scanf("%d" , &n);
+        printf("la media è %d \n", media);
+        media = 
+        b = b + 1;
+    }
+    return(0);
+}
